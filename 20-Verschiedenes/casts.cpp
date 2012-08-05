855c9d78136d003e1d6045de0b76f9fa2cf568e3
Marco Neumann <marco@crepererum.net>
2012-08-05 15:04:21 +0200

    Add casts examples

---
+++ 20-Verschiedenes/casts.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+	virtual void echo() {
+		cout << "A" << endl;
+	}
+};
+
+class B : public A{
+public:
+	void echo() override {
+		cout << "B" << endl;
+	}
+};
+
+void printOnly(const int& i) {
+	int& ref = const_cast<int&>(i);
+	ref++;
+	cout << "I'm a fake printer ;)" << endl;
+}
+
+int main() {
+	// traditional conversion cast
+	float f = 42.42;
+	int i = static_cast<int>(f);
+	cout << i << endl;
+
+	// cast to read/write binary data
+	const char* cstring = "Hello world!";
+	const int* data = reinterpret_cast<const int*>(cstring);
+	cout << *data << endl;
+
+	// const const
+	int test = 1;
+	printOnly(test);
+	cout << test << endl;
+
+	// dynamic cast
+	A* unknown = new B;
+	unknown->echo();
+	B* b = dynamic_cast<B*>(unknown);
+	if (b != nullptr) { // if unkown is not a B object, the pointer should be nullptr
+		b->echo();
+	}
+	delete unknown;
+}
