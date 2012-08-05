73e4d6e8c8d004c5eb378c93c1882587f30fb978
Marco Neumann <marco@crepererum.net>
2012-08-05 12:53:14 +0200

    Add nullptr example

---
+++ 50-Referenzen/nullptr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// stdlib may define NULL as followes:
+#undef NULL
+#define NULL (0)
+
+// int and pointer function
+void foo(int i) {
+	std::cout << "foo(int i)" << std::endl;
+}
+void foo(char* c) {
+	std::cout << "foo(char* c)" << std::endl;
+}
+
+// multiple pointer types
+void bar(char* c) {
+	std::cout << "bar(char* c)" << std::endl;
+}
+void bar(float* f) {
+	std::cout << "bar(float* f)" << std::endl;
+}
+
+int main() {
+	// fun(int i) OR fun(char* c)???
+	//foo(NULL); // error
+
+	// better
+	foo(nullptr);
+
+	// nullptr has no specific type
+	//bar(nullptr); // error
+	bar(static_cast<char*>(nullptr));
+	bar(static_cast<float*>(nullptr));
+}
